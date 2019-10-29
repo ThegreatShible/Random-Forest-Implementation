ec623ff6f9479c8267ec5328242259060a042ab0
camillefossier <camille.fossier@gmail.com>
2019-10-29 11:03:47 +0100

    continued but not working yet

--- src/DecisionTree.R
+++ src/DecisionTree.R
@@ -1,24 +1,46 @@
-decisionTree <- function(attribute, value, childL=NULL, childR=NULL) {
-  tree <- list(
-    attribute=attribute,
-    value=value,
-    childL=childL,
-    childR=childR
+# Returns only a node made of the separation attribute and separation value
+decisionNode <- function(attribute=NA, value=NA, class=NA, children=NA) {
+  node <- list(
+    attribute=attribute, # attribute we check
+    value=value, # value(s) to compare to to choose the branch
+    class=class, # class prediction if we are done
+    children=children
   )
-  class(tree) = "decisionTree"
-  return(tree)
+  class(node) = "decisionTree"
+  return(node)
 }
 
-isLeaf <- function(obj) {
-  UseMethod("isLeaf", obj)
+# Returns entropy of a given dataset and majority class
+entropy <- function(X) {
+  print("TODO")
+  return(list(
+    value=0,
+    majorityClass=0
+  ))
 }
 
-isLeaf.default <- function(obj) {
-  print("default function")
+# Returns a vector list of size (length(j) + 1) containing
+# different subsets of X divided depending on their values
+# for the attribute
+divideDataset <- function(X, attribute, values) {
+  print("TODO")
 }
 
-isLeaf.decisionTree <- function(tree) {
-  return(is.null(tree$childL) & is.null(tree$childR))
+# Takes data matrix X as input and creates a DecisionTree based on it
+decisionTree <- function(X, theta) {
+  node = decisionNode()
+  entropyX = entropy(X)
+  if (entropyX$value < theta) {
+    node$class = entropyX$majorityClass
+    return(node)
+  }
+  else {
+    j <- attributeDivision(X)
+    subNode = decisionNode()
+    subX = divideDataset(X, j)
+    for (i in (1:length(subX))) {
+      node$children[i] = decisionTree(subX[i], theta)
+    }
+    return(node)
+  }
 }
-
-
