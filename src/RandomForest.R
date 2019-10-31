9c3a0f1b99cc30dcdc3661653ec72bfdf9719683
unknown <en_nebbache@esi.dz>
2019-10-31 23:44:14 +0100

    implement train and predict functions for Random Forest

--- src/RandomForest.R
+++ src/RandomForest.R
@@ -1,32 +1,32 @@
 source("src/DecisioNTree.R")
 library(parallel)
 library(foreach)
+library(doParallel)
+
+trainRandomForest <- function(X,Y, nbTrees, theta, r){
+  require(parallel)
+  require(foreach)
+  require(doParallel)
+  vars2export <- c("decisionTree", "entropy","divideDataset", "orderVectorByOther", "separations", "separate",
+                   "is.qualitative", "attributeDivision", "decisionTree", "decisionTree.predict", "decisionNode")
+  registerDoParallel(detectCores()) 
+  data = cbind(X,Y)
+  trainedTrees = foreach(i =  1:nbTrees, .export = vars2export)%dopar%{
+    d = data[sample(nrow(data),replace = TRUE),]
+    localX = d[, -ncol(d)]
+    localY = d[, ncol(d)] 
+    decisionTree(localX,localY,theta, r)
+  }
+  return (trainedTrees)
+}
 
 
-#TODO : Ajouter les imports dans le foreach 
-RandomForest <- setRefClass("RandomForest",
-   fields = list(nbTrees = "integer", theta = "numeric"),
-   methods = list(
-     train = function(X,Y) {
-       data = cbind(X,Y)
-       trainedTrees = foreach(i = 1:nbTrees) %dopar% {
-         d = data[sample(nrow(data)),]
-         localX = d[, -ncol(d)]
-         localY = d[, ncol(d)] 
-         decisionTree(localX,localY,theta)
-       }
-       return (TrainedRandomForest(trees = trainedTrees))
-     }
-     
-     
-   ))
-
-TrainedRandomForest <- setRefClass("TrainedRandomForest",
-   fields = c("trees"),
-   methods = list(
-     predict = function(x) {
-       predictions = foreach(tree = 1:length(trees), .export="decisionTree.predict") %dopar% decisionTree.predict(tree, x)
-       return (which.max(tabulate(predictions)))
-     }
-   ))
-
+predictRandomForest = function(x, Trees) {
+  require(parallel)
+  require(foreach)
+  require(doParallel)
+  vars2export <- c("decisionTree.predict")
+  registerDoParallel(detectCores())
+  predictions = unlist(foreach(tree = trees, .export=vars2export) %dopar% decisionTree.predict(tree, x))
+  return (which.max(tabulate(factor(predictions))))
+}
