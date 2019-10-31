394f0218080c1dda6cbd9af220ef532bc7d2d0e3
camillefossier <camille.fossier@gmail.com>
2019-10-31 09:01:56 +0100

    separated into train and test datasets

--- examples/example2_iris.R
+++ examples/example2_iris.R
@@ -1,13 +1,19 @@
 data("iris")
 source("decisionTree.R")
 iris
-tree = decisionTree(iris[,-ncol(iris)], iris[,ncol(iris)], 0.3)
+
+shuffled = iris[sample(nrow(iris)),]
+trainProportion = (80/100) * nrow(iris)
+train = shuffled[1:trainProportion,]
+test = shuffled[(trainProportion+1):nrow(shuffled),]
+
+tree = decisionTree(train[,-ncol(train)], train[,ncol(train)], 0.3)
 printTree(tree)
 rate=0
-for (i in (1:nrow(iris))) {
-  line = iris[i,]
-  prediction = decisionTree.predict(tree, line[,-ncol(iris)])
-  answer = line[,ncol(iris)]
+for (i in (1:nrow(train))) {
+  line = train[i,]
+  prediction = decisionTree.predict(tree, line[,-ncol(test)])
+  answer = line[,ncol(test)]
   if (answer != prediction) {
     print(line)
     print(p)
@@ -15,5 +21,5 @@ for (i in (1:nrow(iris))) {
     rate = rate + 1
   }
 }
-rate = rate / nrow(iris)
+rate = rate / nrow(test)
 print(rate)
