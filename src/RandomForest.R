cd98e6b54798402afa72b8c69fb5d75c762e0144
unknown <en_nebbache@esi.dz>
2019-11-04 20:21:49 +0100

    parallelisation pour training

--- src/RandomForest.R
+++ src/RandomForest.R
@@ -8,43 +8,47 @@ library(doParallel)
 #Theta is the maximum entropy of a leaf in the trees
 #r is the number of randomly selected variables in each node
 trainRandomForest <- function(X,Y, nbTrees, theta, r){
-  '
+  
   require(parallel)
   require(foreach)
   require(doParallel)
   vars2export <- c("decisionTree", "entropy","divideDataset", "orderVectorByOther", "separations", "separate",
                    "is.qualitative", "attributeDivision", "decisionTree", "decisionTree.predict", "decisionNode")
-  registerDoParallel(detectCores()) 
-  '
+  
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl) 
+  
   data = cbind(X,Y)
-  "
+  
   trainedTrees = foreach(i =  1:nbTrees, .export = vars2export)%dopar%{
     d = data[sample(nrow(data),replace = TRUE),]
     localX = d[, -ncol(d)]
     localY = d[, ncol(d)] 
     decisionTree(localX,localY,theta, r)
   }
-  "
-  trainedTrees = list()
+  
+ ' trainedTrees = list()
   for (i in  1:nbTrees){
     d = data[sample(nrow(data),replace = TRUE),]
     localX = d[, -ncol(d)]
     localY = d[, ncol(d)] 
     trainedTrees[[i]] = decisionTree(localX,localY,theta, r)
-  }
+  }'
+  stopCluster(cl)
   return (trainedTrees)
 }
 
 #Predicts the class of x by the random forest model "trees"
 predictRandomForest = function(x, Trees) {
-  '
+  
   require(parallel)
   require(foreach)
   require(doParallel)
+  #cl <- makeCluster(detectCores())
   vars2export <- c("decisionTree.predict")
-  registerDoParallel(detectCores())
-  predictions = unlist(foreach(tree = trees, .export=vars2export) %dopar% decisionTree.predict(tree, x))
-  '
+  #registerDoParallel(cl)
+  #predictions = unlist(foreach(tree = trees, .export=vars2export) %dopar% decisionTree.predict(tree, x))
+  
   predictions = list()
   for (i in 1:length(Trees)) {
     predictions[[i]] = decisionTree.predict(Trees[[i]], x)
